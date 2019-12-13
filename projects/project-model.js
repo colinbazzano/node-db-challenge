950d7a01025155c711c986208322896fe403eda4
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-12-13 13:11:15 -0500

    post functionality for a project works

--- projects/project-model.js
+++ projects/project-model.js
@@ -2,13 +2,40 @@ const db = require("../db-config.js");
 
 module.exports = {
   getProjects,
-  getResources
+  getResources,
+  getTasks,
+  getProjectById,
+  addProject
 };
 
 function getProjects() {
   return db("projects");
 }
 
+function getProjectById(id) {
+  return db("projects")
+    .where({ id })
+    .first();
+}
+
+function addProject(project) {
+  return db("projects")
+    .insert(project)
+    .then(ids => ({ id: ids[0] }));
+}
+
 function getResources() {
   return db("resources");
+}
+
+function getTasks() {
+  return db("tasks as t")
+    .join("projects as p", "t.project_id", "p.id")
+    .select(
+      "p.name",
+      "p.description as project_description",
+      "t.description",
+      "t.notes",
+      "t.completed"
+    );
 }
--- projects/project-router.js
+++ projects/project-router.js
@@ -4,6 +4,7 @@ const Projects = require("./project-model.js");
 
 const router = express.Router();
 
+// get projects
 router.get("/", (req, res) => {
   Projects.getProjects()
     .then(projects => {
@@ -14,6 +15,38 @@ router.get("/", (req, res) => {
     });
 });
 
+// get project by id
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  Projects.getProjectById(id)
+    .then(project => {
+      project
+        ? res.status(200).json(project)
+        : res
+            .status(404)
+            .json({ message: "Project with specified ID does not exist. " });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "Error getting the project. " });
+    });
+});
+
+// post a project
+router.post("/", (req, res) => {
+  const projectData = req.body;
+
+  Projects.addProject(projectData)
+    .then(project => {
+      res.status(201).json(project);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "Unable to add project." });
+    });
+});
+
+// get resources
 router.get("/resources", (req, res) => {
   Projects.getResources()
     .then(resources => {
@@ -22,6 +55,21 @@ router.get("/resources", (req, res) => {
     .catch(error => {
       console.log(error);
       res.status(500).json({ message: "Failed to get resources, sorry! " });
+    });
+});
+
+// post resource
+
+// get tasks
+router.get("/tasks", (req, res) => {
+  Projects.getTasks()
+    .then(tasks => {
+      console.log(tasks);
+      res.status(200).json(tasks);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "Failed to get tasks, sorry! " });
     });
 });
 
